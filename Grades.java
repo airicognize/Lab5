0b2bbfcfa0ac02f1b66309c7a41aa89826460abf
airi <59892547+airicognize@users.noreply.github.com>
2020-04-21 15:08:59 -0400

    Update Grades.java

--- Grades.java
+++ Grades.java
@@ -10,38 +10,37 @@ public class Grades {
 
         File ft = new File("Grades.txt");
         Scanner sc = new Scanner(ft);
-        int number = 0;
-        int number1 = 0;
+        int largestNumber = 0;
+        int smallestNumber = 0;
         String will0 = sc.next();
         String will1 = sc.next();
         int willGrade = parseInt(will1);
         String william = will0 + " " + willGrade;
-        int largest;
-        int smallest;
+        int largest = Integer.MIN_VALUE;
         int totalGrades = 1;
         int gradesCount = 0;
         String names = null;
         String winner = null;
-
-        System.out.println(william);
+        String looser = null;
+        String namess = "";
 
         do {
             totalGrades++;
             names = sc.next();
             largest = parseInt(String.valueOf(sc.next()));
-            smallest = largest;
             gradesCount += largest;
-            if (number < largest) {
-                number = largest;
+            if (largestNumber < largest) {
+                smallestNumber = largestNumber;
+                if (smallestNumber == largestNumber) {
+                    looser = names;
+                }
+                largestNumber = largest;
                 winner = names;
             }
-
-            if (number < smallest) {
-                smallest = number;
-            }
         } while (sc.hasNext());
-        System.out.println("Highest grade: " + winner + " " + number);
-        System.out.println(smallest);
+
+        System.out.println("Highest grade: " + winner + " " + largestNumber);
+        System.out.println("Smallest Grade: " + looser +  " " + smallestNumber);
         System.out.println("Total grades processed " + totalGrades);
         double average = gradesCount/totalGrades;
         System.out.println("Average of grades is " + average);
