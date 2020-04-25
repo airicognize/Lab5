430ece9880341ca875866607b0c62e8cc20e794f
airi <59892547+airicognize@users.noreply.github.com>
2020-04-25 15:53:02 -0400

    Update Team.java

--- Team.java
+++ Team.java
@@ -10,14 +10,14 @@ public class Team {
 static String name;
 private String officialTeam;
 
-public Team(String TeamName) {
+ public Team(String TeamName) {
     this.name = TeamName;
-}
+ }
 
 public String toString() {
     String officialTeam = "\nTeam name: " + this.name;
     return officialTeam;
-}
+ }
     public boolean equals(Team Team1)  {
         if (this.officialTeam == Team1.officialTeam) {
             return true;
